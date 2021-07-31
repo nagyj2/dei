25dfed2858fe85b9ab0c368e2bccdaa7190fec72
Jason Nagy <jaysun_n@hotmail.com>
2021-07-31 18:21:48 -0400

    added depreciation notice

--- inc/result.h
+++ inc/result.h
@@ -55,6 +55,7 @@ typedef struct result Result;
 
 /** Tests a result struct to ensure relevant fields are filled and others are not.
  * @param[in] res The result struct to be tested.
+ * @deprecated Never used.
  */
 void ensureType(struct result *res);
 
